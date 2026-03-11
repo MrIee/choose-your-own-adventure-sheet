168c8f4b48e113fc2a6af5d9e5abef65c4502b40
mrIee <alexwlee.web@gmail.com>
2026-03-11 22:30:16 +1100

    Remove leftover files from merge

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  prefix: 'tw-',
-  content: ['./public/**/*.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
